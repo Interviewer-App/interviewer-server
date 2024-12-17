19c6c6564bcbdc42b7d6daf62ea300d651033d57
ushansankalpa <ushansan99@gmail.com>
2024-12-17 15:24:55 +0530

    created analyze answer API endpoint

--- src/ai/ai.controller.ts
+++ src/ai/ai.controller.ts
@@ -4,6 +4,7 @@ import { AiService } from './ai.service';
 import { Auth, GetUser } from 'src/auth/decorators';
 import { Role } from '@prisma/client';
 import { GenerateQuestionsDto } from './dto/generate-questions.dto';
+import { AnalyzeQuestionDto } from './dto/analyze-question.dto';
 
 
 @ApiBearerAuth()
@@ -25,6 +26,21 @@ export class AiController {
     @Auth(Role.COMPANY)
     generateQuestions(@Param('id') id: string, @Body() GenerateQuestionsDto: GenerateQuestionsDto) {
         return this.aiService.generateQuestions(id,GenerateQuestionsDto);
+    }
+
+
+    @Post('analiyze-question')
+    @ApiOperation({
+        summary: 'GENERATE QUESTIONS',
+        description: 'Private endpoint to generate questions.'
+    })
+    @ApiResponse({ status: 201, description: 'Created', isArray: true })
+    @ApiResponse({ status: 400, description: 'Bad request' })
+    @ApiResponse({ status: 401, description: 'Unauthorized' })
+    @ApiResponse({ status: 500, description: 'Server error' })             //Swagger
+    @Auth(Role.COMPANY)
+    analyzeQuestion(@Body() AnalyzeQuestionDto: AnalyzeQuestionDto) {
+        return this.aiService.analyzeResponse(AnalyzeQuestionDto);
     }
 
 }
--- src/ai/ai.service.ts
+++ src/ai/ai.service.ts
@@ -3,6 +3,7 @@ import { OpenAI } from 'openai';
 import { GenerateQuestionsDto } from './dto/generate-questions.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { GoogleGenerativeAI } from '@google/generative-ai';
+import { AnalyzeQuestionDto } from './dto/analyze-question.dto';
 
 @Injectable()
 export class AiService {
@@ -24,15 +25,15 @@ export class AiService {
         this.logger.log(`POST: interview/generate-and-add-questions: Started`);
         const model = this.genAI.getGenerativeModel({ model: "gemini-2.0-flash-exp" });
         const generationConfig = {
-            temperature: 1,               
-            topP: 0.95,                  
-            topK: 40,                    
-            maxOutputTokens: 8192,        
+            temperature: 1,
+            topP: 0.95,
+            topK: 40,
+            maxOutputTokens: 8192,
             responseMimeType: "application/json",
-          };
+        };
 
         try {
-            const prompt = `Generate 5 interview questions for a ${dto.skillLevel} ${dto.jobRole} role. Provide the question and specify the type: 'open-ended' or 'coding'. Format the response as JSON.`;
+            const prompt = `Generate 5 interview questions for a ${dto.skillLevel} ${dto.jobRole} role. Provide the question, specify the type: 'open-ended' or 'coding' and estimate time for the answering each question. Format the response as JSON.`;
 
             //   const response = await this.openai.chat.completions.create({
             //     model: "gpt-3.5-turbo",
@@ -85,6 +86,74 @@ export class AiService {
                 `POST: interview/generate-and-add-questions: Error occurred: ${error.message}`
             );
             this.prismaErrorHandler(error, "POST", id);
+            throw new InternalServerErrorException("Server error occurred");
+        }
+    }
+
+    async analyzeResponse(dto: AnalyzeQuestionDto): Promise<any> {
+        this.logger.log(`POST: interview/generate-and-add-questions: Started`);
+        const model = this.genAI.getGenerativeModel({ model: "gemini-2.0-flash-exp" });
+        const generationConfig = {
+            temperature: 1,
+            topP: 0.95,
+            topK: 40,
+            maxOutputTokens: 8192,
+            responseMimeType: "application/json",
+        };
+
+        try {
+            const prompt = `
+                    Analyze the following interview response and provide:
+                    1. Relevance score (0-100)
+                    2. Key strengths
+                    3. Areas of improvement
+                    4. Alignment with job requirements
+                    5. Another related  3 questions based on answer
+
+                    Question: "${dto.question}"
+                    Candidate's Response: "${dto.answer}"
+                    
+                    Provide the result as a JSON object in the following format:
+                    {
+                    "relevanceScore": number,
+                    "keyStrengths": string[],
+                    "areasOfImprovement": string[],
+                    "alignment": string,
+                    "followUpQuestions":string[]
+                    }
+                `;
+
+            //   const response = await this.openai.chat.completions.create({
+            //     model: "gpt-3.5-turbo",
+            //     messages: [{ role: "user", content: prompt }],
+            //     response_format: { type: "json_object" },
+            //   });
+
+            //   const content = response.choices[0]?.message?.content;
+
+            const result = await model.generateContent({
+                contents: [{ role: "user", parts: [{ text: prompt }] }],
+                generationConfig: generationConfig,
+            });
+            const content = result.response.text();
+            let analiyze = [];
+
+            try {
+                analiyze = JSON.parse(content);
+            } catch (parseError) {
+                this.logger.error(
+                    "Failed to parse OpenAI response into JSON format",
+                    parseError
+                );
+                throw new InternalServerErrorException("Invalid response from OpenAI");
+            }
+
+            return analiyze;
+        } catch (error) {
+            this.logger.error(
+                `POST: interview/generate-and-add-questions: Error occurred: ${error.message}`
+            );
+            // this.prismaErrorHandler(error, "POST", id);
             throw new InternalServerErrorException("Server error occurred");
         }
     }
