c63f042b64e1fd249e9eb40ad3a6447f3138bf82
ushansankalpa <ushansan99@gmail.com>
2024-12-28 10:09:50 +0530

    Refactor AI service and controller formatting and structure
    Reformatted code for better readability, adjusted indentation, and updated quotes to maintain consistency. Improved structured exception handling and logging, ensuring clarity and error traceability in the AI service.

--- src/ai/ai.controller.ts
+++ src/ai/ai.controller.ts
@@ -5,7 +5,7 @@ import { Auth, GetUser } from 'src/auth/decorators';
 import { Role } from '@prisma/client';
 import { GenerateQuestionsDto } from './dto/generate-questions.dto';
 import { AnalyzeQuestionDto } from './dto/analyze-question.dto';
-import { AnalyzeCandidateDto } from "./dto/analyze-candidate.dto";
+import { AnalyzeCandidateDto } from './dto/analyze-candidate.dto';
 
 
 @ApiBearerAuth()
@@ -17,7 +17,7 @@ export class AiController {
 
     @Post('generate-questions/:interviewId')
     @ApiOperation({
-        summary: 'GENERATE QUESTIONS',
+    summary: 'GENERATE QUESTIONS',
         description: 'Private endpoint to generate questions.'
     })
     @ApiResponse({ status: 201, description: 'Created', isArray: true })
@@ -55,7 +55,7 @@ export class AiController {
     @ApiResponse({ status: 500, description: 'Server error' })             //Swagger
     @Auth(Role.COMPANY)
     analyzeCandidate(@Body() AnalyzecandidateDto: AnalyzeCandidateDto) {
-        return this.aiService.analyzeCandidate(AnalyzecandidateDto);
+    return this.aiService.analyzeCandidate(AnalyzecandidateDto);
     }
 
 }
